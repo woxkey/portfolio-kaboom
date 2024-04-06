d27c9476090a5eb2529def9e54c5bd6901a7d184
Alen <bolatovalen@gmail.com>
2024-04-05 20:03:59 -0400

    inital commit

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/woxkey" target="_blank">Github</a>!`,
+  "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it.`,
+  "sofa-table": `That's my sofa. I like to relax here and watch YouTube.`,
+  tv: `That's my TV.`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/woxkey" target="_blank">Check it out?</a>
+  Contact me at bolatovalen@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS.`,
+  library: `There are a lot of programming books on my shelves...`,
+  exit: `If you want to exit Alen's portfolio, just close the tab.`,
+};
